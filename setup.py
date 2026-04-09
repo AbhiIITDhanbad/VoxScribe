a96244f4f89d347999bd85a9fa8a4b0282e3cc19
AbhiIITDhanbad <abhirupiitian@gmail.com>
2026-04-09 21:55:57 +0530

    .

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages 
+
+
+setup(
+    name = "STT",
+    version="0.0.1",
+    author="Abhirup Sarkar",
+    packages=find_packages(),
+    install_requires = []
+)
+
